5b84112f24bcda5c8029663b93d10150b98fb6ff
manul <manuuva5@gmail.com>
2025-03-08 20:26:11 -0300

    add delete behavior

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 function initNumbers() {
-    numbers = document.querySelectorAll('.number')
-    display = document.querySelector('.display')
+    const numbers = document.querySelectorAll('.number')
+    const display = document.querySelector('.display')
     numbers.forEach(element => {
-        console.log(display.textContent.length)
         element.addEventListener('click', () => {
             display.innerText.length < 12 ? display.textContent += element.textContent : display.textContent += ''})
     });
 }
 
 function initAC() {
-    ac = document.querySelector('#ac')
-    display = document.querySelector('.display')
+    const ac = document.querySelector('#ac')
+    const display = document.querySelector('.display')
     ac.addEventListener('click', () => display.textContent = '')
 }
 
+function initDel() {
+    const del = document.querySelector('.del')
+    const display = document.querySelector('.display')
+    del.addEventListener('click', () => display.textContent = display.innerText.slice(0, display.innerText.length-1))
+}
+
+// functioninitOp() {
+//     op
+// }
+
+
 initNumbers()
 initAC()
+initDel()
